82da221b58c7291729581b90d9a88df7c40e5c13
willjrmonteiro <william.monteiro@grupoboticario.com.br>
2023-04-24 19:34:06 -0300

    add var env cors

--- server.go
+++ server.go
@@ -10,20 +10,28 @@ import (
 
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+
+	"github.com/rs/cors"
 )
 
 const defaultPort = "8080"
 
 func main() {
 	port := os.Getenv("PORT")
+	allowedCorsOrigins := os.Getenv("ALLOWED_CORS_ORIGINS")
 	if port == "" {
 		port = defaultPort
 	}
 
 	srv := handler.NewDefaultServer(generated.NewExecutableSchema(generated.Config{Resolvers: &graphql.Resolver{}}))
 
-	http.Handle("/", playground.Handler("GraphQL playground", "/query"))
-	http.Handle("/query", srv)
+	corsOptions := cors.Options{
+		AllowedOrigins: []string{allowedCorsOrigins},
+	}
+	corsMiddleware := cors.New(corsOptions).Handler
+
+	http.Handle("/", corsMiddleware(playground.Handler("GraphQL playground", "/query")))
+	http.Handle("/query", corsMiddleware(srv))
 
 	log.Printf("connect to http://localhost:%s/ for GraphQL playground", port)
 	log.Fatal(http.ListenAndServe(":"+port, nil))
